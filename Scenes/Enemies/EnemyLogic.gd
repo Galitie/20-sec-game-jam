6c6634ee36ec10701b7d38bc4381edc18b7345bb
Galitie <galitdances@gmail.com>
2023-11-08 00:58:41 -0500

    can now destroy enemies

--- Scenes/Enemies/EnemyLogic.gd
+++ Scenes/Enemies/EnemyLogic.gd
@@ -1,17 +1,22 @@
 extends PathFollow2D
 
-@export_category("Enemy Variables")
-@export var enemy_health: int = 3
-@export var enemy_speed: int = 150
+var enemy_speed: int = 150
+var hp = 5
 
-func _process(delta):
-	pass
-	# if enemy_health <= 0:
-	# die
-	
+
 func _physics_process(delta):
 	move(delta)
+
 
 func move(delta):
 	set_progress(get_progress() + enemy_speed * delta)
 
+
+func on_hit(damage):
+	hp -= damage
+	if hp <= 0:
+		on_destroy()
+
+
+func on_destroy():
+	self.queue_free()
