e008d819fa7f3ad5dd257359054f1afbb8c36aa4
Galitie <galitdances@gmail.com>
2023-11-07 23:08:23 -0500

    added tower range indicator when building

--- Prototype/UI.gd
+++ Prototype/UI.gd
@@ -1,13 +1,22 @@
 extends CanvasLayer
 
+var tower_range = 350
 
 func set_tower_preview(tower_type, mouse_position):
 	var drag_tower = load("res://Scenes/Towers/" + tower_type + ".tscn").instantiate()
 	drag_tower.set_name("DragTower")
 	drag_tower.modulate = Color("#14FF0084")
 	
+	var range_texture = Sprite2D.new()
+	var scaling = tower_range / 600.0
+	range_texture.scale = Vector2(scaling, scaling)
+	var texture = load("res://Assets/UI/range_overlay.png")
+	range_texture.texture = texture
+	range_texture.modulate = Color("#14FF0084")
+	
 	var control = Control.new()
 	control.add_child(drag_tower, true)
+	control.add_child(range_texture, true)
 	control.global_position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control, true)
@@ -17,3 +26,4 @@ func update_tower_preview(new_position, color):
 	get_node("TowerPreview").global_position = new_position
 	if get_node("TowerPreview/DragTower").modulate != Color(color):
 		get_node("TowerPreview/DragTower").modulate = Color(color)
+		get_node("TowerPreview/Sprite2D").modulate = Color(color)
