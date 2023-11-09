cbab7fc84597cddcdb2fa791db685242c0ee164d
Galit Weinfeld <galitdances@gmail.com>
2023-11-09 16:21:04 -0500

    made enemies spawn faster and more of them

--- Globals.gd
+++ Globals.gd
@@ -1,29 +1,45 @@
 extends Node
 
 # Change the rate enemies appear
-var sec_between_enemies = 1
+var sec_between_enemies = .25
 
 # enemies and towers are objects, add an object to the arrays below to add more
 # enemies have a default health of 3 and a default speed of 150
 # they will spawn in order from the start of the array to the end
 # currently they just keep looping when they reach the end of the path
 var enemy_data = [ 
-	{"hp": 3, "speed": 150},
-	{"hp": 3, "speed": 250},
-	{"hp": 3, "speed": 100},
-	{"hp": 3, "speed": 350},
-	{"hp": 3, "speed": 500},
-	{"hp": 3, "speed": 100},
-	{"hp": 3, "speed": 100},
-	{"hp": 3, "speed": 700},
-	{"hp": 3, "speed": 200},
-	{"hp": 3, "speed": 250},
-	{"hp": 3, "speed": 100},
-	{"hp": 3, "speed": 150},
-	{"hp": 3, "speed": 350},
-	{"hp": 3, "speed": 100},
-	{"hp": 3, "speed": 100},
-	{"hp": 3, "speed": 700},
+	{"hp": 2, "speed": 150},
+	{"hp": 2, "speed": 350},
+	{"hp": 2, "speed": 200},
+	{"hp": 2, "speed": 350},
+	{"hp": 2, "speed": 500},
+	{"hp": 2, "speed": 200},
+	{"hp": 2, "speed": 200},
+	{"hp": 2, "speed": 700},
+	{"hp": 2, "speed": 200},
+	{"hp": 2, "speed": 250},
+	{"hp": 2, "speed": 200},
+	{"hp": 2, "speed": 250},
+	{"hp": 2, "speed": 250},
+	{"hp": 2, "speed": 100},
+	{"hp": 2, "speed": 100},
+	{"hp": 2, "speed": 700},
+	{"hp": 2, "speed": 150},
+	{"hp": 2, "speed": 250},
+	{"hp": 2, "speed": 600},
+	{"hp": 2, "speed": 350},
+	{"hp": 2, "speed": 500},
+	{"hp": 2, "speed": 400},
+	{"hp": 2, "speed": 100},
+	{"hp": 2, "speed": 600},
+	{"hp": 2, "speed": 200},
+	{"hp": 2, "speed": 650},
+	{"hp": 2, "speed": 100},
+	{"hp": 2, "speed": 550},
+	{"hp": 2, "speed": 350},
+	{"hp": 2, "speed": 500},
+	{"hp": 2, "speed": 100},
+	{"hp": 2, "speed": 700},
 ]
 
 # only 3 towers available for prototype, don't add more but feel free to change the values
